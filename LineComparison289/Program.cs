f6d7f134c4de55071ad4b84c28b9684c15dca53a
Sachinmahajana <sachinmahajan782@gmail.com>
2023-06-28 10:35:06 +0530

    [Sachin] Adding Welcome Message

---
+++ LineComparison289/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LineComparison289
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Line Comparison Problem");
+        }
+    }
+}
