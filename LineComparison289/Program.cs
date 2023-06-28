8845532c139fc2c8edd26d8a0c0f0edec285be78
Sachinmahajana <sachinmahajan782@gmail.com>
2023-06-28 10:42:12 +0530

    [Sachin] Adding UC2-CheckEquality

--- LineComparison289/Program.cs
+++ LineComparison289/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Line Comparison Problem");
-            //UC1-Lineoflength
+            //UC2-Checkequality
             Console.WriteLine("Enter the value of x1 and y1");
             int x1 = Convert.ToInt32(Console.ReadLine());
             int y1 = Convert.ToInt32(Console.ReadLine());
@@ -15,6 +15,25 @@
 
             double length = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
             Console.WriteLine("Line of length is: " + length);
+
+            Console.WriteLine("Enter the value of x3 and y3");
+            int x3 = Convert.ToInt32(Console.ReadLine());
+            int y3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of x4 and y4");
+            int x4 = Convert.ToInt32(Console.ReadLine());
+            int y4 = Convert.ToInt32(Console.ReadLine());
+
+            double line2 = Math.Sqrt((x3 - y3) ^ 2 + (x4 - y4) ^ 2);
+            Console.WriteLine("Line of length is: " + line2);
+
+            if (length.Equals(line2))
+            {
+                Console.WriteLine("Both line length are equal");
+            }
+            else
+            {
+                Console.WriteLine("Line length are not equal");
+            }
         }
     }
 }
