4f2b284c3e6fff4dc6999e0559eb267c37c51124
Sachinmahajana <sachinmahajan782@gmail.com>
2023-06-28 10:38:03 +0530

    [Sachin] Adding UC1-LineLength

--- LineComparison289/Program.cs
+++ LineComparison289/Program.cs
@@ -5,6 +5,16 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Line Comparison Problem");
+            //UC1-Lineoflength
+            Console.WriteLine("Enter the value of x1 and y1");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of x2 and y2");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+            double length = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+            Console.WriteLine("Line of length is: " + length);
         }
     }
 }
